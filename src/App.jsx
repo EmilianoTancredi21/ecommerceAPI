73d02260391de5698c85f7755bb0f3653e78e8d6
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-04-18 17:05:28 -0300

    Importamos las vistas y componentes, configurando el enrutamiento.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Routes
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+
+// Components
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="overflow-hidden">APP</div>;
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Details />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
