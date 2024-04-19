6e83ea1256144c5b840d357c0d16207824bf2901
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-04-19 01:03:37 -0300

    Se ha implementado ProductProvider utilizando useContext y useState para manejar el estado global de los productos. Este proveedor realiza una petición a la API de Fake Store para obtener los productos y los almacena en el estado. Luego, estos datos se proveen a través del contexto a todos los componentes que lo consuman, permitiendo un acceso centralizado y eficiente a los datos de los productos en la aplicación.

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,5 +1,27 @@
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+import { useState, useEffect, createContext } from "react";
+
+// creamos el contexto
+
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
 
-export default ProductContext;
+export default ProductProvider;
