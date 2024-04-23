8b36b051f1400d03e63c4492f66baf9851c14b54
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-04-22 23:11:06 -0300

    Se añade el Footer a la aplicación, full responsive utilizando tailwind.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import Logo from "../img/logo_footer.svg";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=" shadow dark:bg-primary">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link
+            to="/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src={Logo}
+              className="h-8 fill-white stroke-white"
+              alt="Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-gray-500">
+              Fake Store
+            </span>
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <Link to="/" className="hover:underline me-4 md:me-6">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" className="hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024 Tancredi Emiliano™
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
