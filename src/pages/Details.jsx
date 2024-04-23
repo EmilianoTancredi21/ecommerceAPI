0b22c58c4ed35e425d6c940c1e4b45977d8d16eb
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-04-22 23:14:22 -0300

    Se implementa la vista full responsive de detalle de producto.

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,59 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
+import { ProductContext } from "../contexts/ProductContext";
+import { Link } from "react-router-dom";
+// import { SlArrowLeftCircle } from "react-icons/sl";
+import { GoArrowLeft } from "react-icons/go";
+
 const Details = () => {
-  return <div>Details</div>;
+  const { id } = useParams();
+  const { products } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
+
+  const product = products.find((item) => {
+    return item.id === parseInt(id);
+  });
+
+  if (!product) {
+    return (
+      <section className="h-screen flex justify-center items-center">
+        Loading...
+      </section>
+    );
+  }
+
+  const { title, price, image, description } = product;
+
+  return (
+    <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
+      <div className="container mx-auto">
+        <Link to={"/"}>
+          <GoArrowLeft className="w-[32px] h-[32px] fixed" />
+        </Link>
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
+            <img className="max-w-[200px] lg:max-w-sm" src={image} alt="" />
+          </div>
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">
+              {title}
+            </h1>
+            <div className="text-xl text-red-500 font-medium mb-6">
+              $ {price}
+            </div>
+            <p className="mb-8">{description}</p>
+            <button
+              onClick={() => addToCart(product, product.id)}
+              className="bg-primary py-4 px-8 text-white"
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Details;
