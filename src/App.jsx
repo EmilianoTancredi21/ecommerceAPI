65290116ce2feec665a982b7b5141fafaca747bd
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-05-08 21:03:06 -0300

    Añadimos la Ruta checkout para finalizar la compra y el componente filter para productos, ambos funcionando en su totalidad.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Routes
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import CheckOut from "./pages/CheckOut";
 
 // Components
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import FilterSidebar from "./components/FilterSidebar";
 
 function App() {
   return (
@@ -17,8 +19,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<Details />} />
+          <Route path="/checkout" element={<CheckOut />} />
         </Routes>
         <Sidebar />
+        <FilterSidebar />
         <Footer />
       </Router>
     </div>
