9d6d54e039963643e4e7fc4ef9a8271c0d380df2
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-05-08 21:03:54 -0300

    Agregamos el FilterProvider a la app para que este disponible globalmente y lo podamos utilizar en cualquier parte de la app.

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,22 @@ import "./index.css";
 import ProductProvider from "./contexts/ProductContext.jsx";
 import SidebarProvider from "./contexts/SideBarContext.jsx";
 import CartProvider from "./contexts/CartContext.jsx";
+import { FilterProvider } from "./contexts/FilterContext.jsx";
+
 import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <SidebarProvider>
-      <CartProvider>
-        <ProductProvider>
-          <React.StrictMode>
-            <App />
-          </React.StrictMode>
-        </ProductProvider>
-      </CartProvider>
+      <FilterProvider>
+        <CartProvider>
+          <ProductProvider>
+            <React.StrictMode>
+              <App />
+            </React.StrictMode>
+          </ProductProvider>
+        </CartProvider>
+      </FilterProvider>
     </SidebarProvider>
     <Toaster richColors />
   </>
