cee8606d9496bebf071e36b22eaf73f9cda142d8
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-05-08 21:12:22 -0300

    Se ha actualizado el componente Home para mostrar los resultados de búsqueda si existen.
    Cambios en Home.js:
    
    - Se ha importado useContext desde "react".
    - Se ha importado ProductContext desde "../contexts/ProductContext".
    - Se ha actualizado el componente Home para utilizar el contexto ProductContext.
    - Se ha añadido un nuevo estado searchResults al destructurar el contexto ProductContext.
    - Se ha actualizado la lógica de productList para mostrar los resultados de búsqueda si existen, de lo contrario muestra todos los productos.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import Product from "../components/Product";
 import Hero from "../components/Hero";
 
 const Home = () => {
-  const { products } = useContext(ProductContext);
+  const { products, searchResults } = useContext(ProductContext);
+
+  const productList = searchResults.length > 0 ? searchResults : products;
 
   return (
     <div>
@@ -12,7 +14,7 @@ const Home = () => {
       <section className="py-16">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
-            {products.map((product) => {
+            {productList.map((product) => {
               return <Product product={product} key={product.id} />;
             })}
           </div>
