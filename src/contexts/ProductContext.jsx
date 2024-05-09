fbd72339e60c53c06af901b3ea9cda7e2bcd6221
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2024-05-08 21:10:38 -0300

    Se ha actualizado el proveedor de productos para incluir la funcionalidad de búsqueda.
    Cambios en ProductProvider.js:
    
    - Se ha importado useState, useEffect y createContext desde "react".
    - Se ha definido un contexto llamado ProductContext utilizando createContext().
    - Se ha agregado un nuevo estado llamado searchResults utilizando useState, con un valor inicial vacío.
    - Se ha añadido setSearchResults como parte del valor proporcionado por el contexto.
    - Dentro del useEffect, se ha actualizado fetchProducts para establecer los resultados de búsqueda en searchResults después de obtener los datos de la API.
    - Se ha envuelto children dentro de ProductContext.Provider, proporcionando el valor del contexto con products, searchResults y setSearchResults.

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -6,11 +6,13 @@ export const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
+
       setProducts(data);
     };
 
@@ -18,7 +20,9 @@ const ProductProvider = ({ children }) => {
   }, []);
 
   return (
-    <ProductContext.Provider value={{ products }}>
+    <ProductContext.Provider
+      value={{ products, searchResults, setSearchResults }}
+    >
       {children}
     </ProductContext.Provider>
   );
